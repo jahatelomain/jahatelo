66b708bb953575c3fc4110ea645e6417b8639a43
Jahatelo <jahatelomain@gmail.com>
2025-12-11 17:19:34 -0300

    chore: remove old sqlite migrations

--- web/jahatelo-web/prisma/migrations/20251211012708_commercial_contacts/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE "Motel" ADD COLUMN "adminContactEmail" TEXT;
-ALTER TABLE "Motel" ADD COLUMN "adminContactName" TEXT;
-ALTER TABLE "Motel" ADD COLUMN "adminContactPhone" TEXT;
-ALTER TABLE "Motel" ADD COLUMN "operationsContactEmail" TEXT;
-ALTER TABLE "Motel" ADD COLUMN "operationsContactName" TEXT;
-ALTER TABLE "Motel" ADD COLUMN "operationsContactPhone" TEXT;
